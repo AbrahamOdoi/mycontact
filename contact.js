8a82f327979c584fa09dfdfa97ef80e82eba38af
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-03-08 13:15:33 +0000

    sdjf

--- contact.js
+++ contact.js
@@ -1,38 +1,23 @@
-// (function() {
-//
-// document.addEventListener('deviceready', onDeviceReady.bind(this), false);
-// function onDeviceReady() {
-//
-// document.getElementById("search").onclick = function() {
-// navigator.contacts.pickContact(function(contact) {
-// alert('The following contact has been selected:' + JSON.stringify(contact));
-//
-// document.getElementById("search").value = contact.phoneNumbers[0].value;
-// alert(contact.phoneNumbers[0].value);
-// var countMyContacts = contactCount;
-// alert(countMyContacts);
-//
-// document.getElementById("displayContacts").html(contact.phoneNumbers[0].value)
-//
-// }, function(err) {
-// alert('Error: ' + err);
-// });
-// }
-// } ;
-// })();
-
 (function() {
 
 	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 	function onDeviceReady() {
-		alert('yes');
-		document.getElementById("search").onclick = function() {
-			navigator.contactsPhoneNumbers.list(function(contacts) {
-				alert(contacts.length + ' contacts found')
 
-			}, function(error) {
-				console.error(error);
+		document.getElementById("search").onclick = function() {
+			navigator.contacts.pickContact(function(contact) {
+				alert(contact.length);
+				// alert('The following contact has been selected:' + JSON.stringify(contact));
+
+				document.getElementById("search").value = contact.phoneNumbers[0].value;
+				alert(contact.phoneNumbers[0].value);
+				var countMyContacts = contactCount;
+				alert(countMyContacts);
+
+				document.getElementById("displayContacts").html(contact.phoneNumbers[0].value)
+
+			}, function(err) {
+				alert('Error: ' + err);
 			});
-		};
-	};
+		}
+	} ;
 })();
