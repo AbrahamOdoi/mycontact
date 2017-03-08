173c5db51bbbbdbb6435d1af5c856872ebde9796
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-03-08 12:54:05 +0000

    hsd

--- contact.js
+++ contact.js
@@ -25,21 +25,11 @@
 
 	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 	function onDeviceReady() {
-
+		alert('yes');
 		document.getElementById("search").onclick = function() {
 			navigator.contactsPhoneNumbers.list(function(contacts) {
-				alert('yes');
-				// console.log(contacts.length + ' contacts found');
 				alert(contacts.length + ' contacts found')
 
-				for (var i = 0; i < contacts.length; i++) {
-					// console.log(contacts[i].id + " - " + contacts[i].displayName);
-					for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
-						var phone = contacts[i].phoneNumbers[j];
-						// console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")");
-					}
-					alert(phone);
-				}
 			}, function(error) {
 				console.error(error);
 			});
