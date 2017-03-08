a0393a76fb899729251c65bf38dbfa16d9aa5417
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-03-08 13:34:29 +0000

    Skiff

--- contact.js
+++ contact.js
@@ -4,12 +4,13 @@
 	function onDeviceReady() {
 
 		document.getElementById("search").onclick = function() {
-			navigator.contacts.pickContact(function(contact) {
+			navigator.contacts.list(function(contact) {
+			// navigator.contacts.pickContact(function(contact) {
 				alert(contacts.length);
-				// alert('The following contact has been selected:' + JSON.stringify(contact));
+				alert('The following contact has been selected:' + JSON.stringify(contact));
 
 				document.getElementById("search").value = contact.phoneNumbers[0].value;
-				// alert(contact.phoneNumbers[0].value);
+				alert(contact.phoneNumbers[0].value);
 				var countMyContacts = contactCount;
 				alert(countMyContacts);
 
