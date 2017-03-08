8dd247c281ee7a685b1a621b4e8640fa7124412e
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-03-08 12:26:52 +0000

    Sfdjk

--- contact.js
+++ contact.js
@@ -1,26 +1,25 @@
 // (function() {
-// 
-	// document.addEventListener('deviceready', onDeviceReady.bind(this), false);
-	// function onDeviceReady() {
-// 
-		// document.getElementById("search").onclick = function() {
-			// navigator.contacts.pickContact(function(contact) {
-				// alert('The following contact has been selected:' + JSON.stringify(contact));
-// 
-				// document.getElementById("search").value = contact.phoneNumbers[0].value;
-				// alert(contact.phoneNumbers[0].value);
-				// var countMyContacts = contactCount;
-				// alert(countMyContacts);
-// 
-				// document.getElementById("displayContacts").html(contact.phoneNumbers[0].value)
-// 
-			// }, function(err) {
-				// alert('Error: ' + err);
-			// });
-		// }
-	// } ;
+//
+// document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+// function onDeviceReady() {
+//
+// document.getElementById("search").onclick = function() {
+// navigator.contacts.pickContact(function(contact) {
+// alert('The following contact has been selected:' + JSON.stringify(contact));
+//
+// document.getElementById("search").value = contact.phoneNumbers[0].value;
+// alert(contact.phoneNumbers[0].value);
+// var countMyContacts = contactCount;
+// alert(countMyContacts);
+//
+// document.getElementById("displayContacts").html(contact.phoneNumbers[0].value)
+//
+// }, function(err) {
+// alert('Error: ' + err);
+// });
+// }
+// } ;
 // })();
-
 
 (function() {
 
@@ -28,22 +27,22 @@
 	function onDeviceReady() {
 
 		document.getElementById("search").onclick = function() {
-			navigator.contactsPhoneNumbers.list(function(contacts) { 
+			navigator.contactsPhoneNumbers.list(function(contacts) {
 				alert('yes');
-		      // console.log(contacts.length + ' contacts found');
-		      	alert(contacts.length + ' contacts found')
-		      	
-		      for(var i = 0; i < contacts.length; i++) {
-		         // console.log(contacts[i].id + " - " + contacts[i].displayName);
-		         for(var j = 0; j < contacts[i].phoneNumbers.length; j++) {
-		            var phone = contacts[i].phoneNumbers[j];
-		            // console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")");
-		         }
-		         alert(phone);
-		      }
-		   }, function(error) {
-		      console.error(error);
-		   });
-			
+				// console.log(contacts.length + ' contacts found');
+				alert(contacts.length + ' contacts found')
+
+				for (var i = 0; i < contacts.length; i++) {
+					// console.log(contacts[i].id + " - " + contacts[i].displayName);
+					for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
+						var phone = contacts[i].phoneNumbers[j];
+						// console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")");
+					}
+					alert(phone);
+				}
+			}, function(error) {
+				console.error(error);
+			});
+		};
 	};
 })();
