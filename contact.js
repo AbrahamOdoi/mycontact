7a9e8f39794092356d1bbb2c80fbc78e8e01e9f2
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-03-08 15:03:51 +0000

    jsdk
    jdfsk

--- contact.js
+++ contact.js
@@ -10,8 +10,9 @@
 				document.getElementById("search").value = contact.phoneNumbers[0].value;
 				alert('myt ' + contact.phoneNumbers[0].value);
 
-				document.getElementById("displayContacts").html = contact.phoneNumbers[0].value;
-				document.getElementById("displayContacts").value = '2' + contact.phoneNumbers[0].value;
+				var contactsDisplayed = document.getElementById("displayContacts").value;
+
+				document.getElementById("displayContacts").value = contactsDisplayed + ',' + contact.phoneNumbers[0].value;
 
 			}, function(err) {
 				alert('Error: ' + err);
