b392d05b101e465b32243fffbb1fc98c5f4c3880
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-03-08 13:46:44 +0000

    Ndseqq

--- contact.js
+++ contact.js
@@ -4,16 +4,14 @@
 	function onDeviceReady() {
 
 		document.getElementById("search").onclick = function() {
-			navigator.contacts.list(function(contact) {
-			// navigator.contacts.pickContact(function(contact) {
-				alert(contacts.length);
+			navigator.contacts.pickContact(function(contact) {
 				alert('The following contact has been selected:' + JSON.stringify(contact));
 
 				document.getElementById("search").value = contact.phoneNumbers[0].value;
 				alert(contact.phoneNumbers[0].value);
-				var countMyContacts = contactCount;
-				alert(countMyContacts);
- 
+				
+				
+
 				document.getElementById("displayContacts").html(contact.phoneNumbers[0].value)
 
 			}, function(err) {
