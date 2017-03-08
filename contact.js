83371be7fe619353cdcdea8d0bde47e1f0c26db0
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-03-08 14:13:39 +0000

    uw

--- contact.js
+++ contact.js
@@ -10,7 +10,8 @@
 				document.getElementById("search").value = contact.phoneNumbers[0].value;
 				alert('myt ' + contact.phoneNumbers[0].value);
 
-				document.getElementById("displayContacts").html(contact.phoneNumbers[0].value);
+				document.getElementById("displayContacts").html = contact.phoneNumbers[0].value;
+				document.getElementById("displayContacts").value = '2' + contact.phoneNumbers[0].value;
 
 			}, function(err) {
 				alert('Error: ' + err);
